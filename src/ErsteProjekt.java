a30e1f90964d45acb65ad71e4fe3b513131905ae
Dr. Dillinger <peter.dillinger@srh.de>
2023-03-23 13:56:14 +0100

    Namensänderung und zwei Variablen, die etwas berechnen und das Ergebnis ausgeben.

---
+++ src/ErsteProjekt.java
@@ -0,0 +1,15 @@
+public class ErsteProjekt {
+    public static void main(String[] args) {
+        int a; // Deklaration der Variable a
+        int b;
+
+        a = 10; // Initialisierung (erste Wertzuweisung)
+        b = 15;
+
+        int c = 17; // Deklaration und Initialisierung in einem
+
+        c = a + b;  // jetzt rechnen wir mal ...
+
+        System.out.println( "c = " + c );
+    }
+}
