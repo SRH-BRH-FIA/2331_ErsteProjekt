a795cde700510ec8b92867ae19fe962915b4e728
Dr. Peter Dillinger <peter.dillinger@srh.de>
2023-04-20 10:04:37 +0200

    Erstes Programm mit Euklid als EVA ausgelegt, Abfrage der Werte für a und b, Berechnung und Ausgabe des Ergebnisses.

--- src/ErsteProjekt.java
+++ src/ErsteProjekt.java
@@ -1,10 +1,17 @@
+import java.util.Scanner;
+
 public class ErsteProjekt {
     public static void main(String[] args) {
         int a; // Deklaration der Variable a
         int b;
 
-        a = 9; // Initialisierung (erste Wertzuweisung)
-        b = 15;
+        Scanner eingabe = new Scanner(System.in);
+
+        System.out.print("Bitte Wert für 'a' eingeben: ");
+        a = eingabe.nextInt();
+
+        System.out.print("Bitte Wert für 'b' eingeben: ");
+        b = eingabe.nextInt();
 
         // Euklid einfügen!
         while (a != b) {
